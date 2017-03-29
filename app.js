857114438d6e7d145e1f21fdb943e340dbedb5f3
Anupam Ghosh <anghosh@arw.pvt>
2017-03-29 14:03:48 +0530

    message is identified by romID

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 'use strict'
 
+class ChatData {
+	constructor(room, owner, message) {
+		this.room = room;
+		this.time = new Date().getTime();
+		this.owner = owner; // chat author
+		this.message = message
+	}
+	toString() {
+		return `room=${this.room} by=${this.owner} message=${this.message}`
+	}
+}
 class User {
 	constructor(id, socket, name) {
-		this.id = id.substring(2); // removes `/#` prefix
+		this.id = id;
 		this.name = name;
 		this.socket = socket;
 		this.rooms = [];
+		this.buddies = {};
 	}
-
 	toString() {
 		return `id=${this.id} name=${this.name} rooms=${JSON.stringify(this.rooms)}`
 	}
@@ -27,47 +38,89 @@ app.get('/', function(req, res) {
 });
 app.use(express.static('public'));
 
-var userNames = {};
+var publicRoom = {
+	id	: 'public-room',
+	name: 'Public Room',
+	room: 'default',
+};
+var userNames = {}
+var chatHistory = {}
 io.on('connection', function(socket) {
-	var user = new User(socket.id, socket, 'Guest');
+	var user = new User(socket.id.substring(2), socket, 'Guest'); // removes `/#` prefix
+	socket.emit('init', {
+		publicRoom: publicRoom
+	});
+	socket.join(publicRoom.room);
+	user.rooms.push(publicRoom.room);
+
+	chatHistory[publicRoom.room] = [];
+	user.buddies[publicRoom.id] = publicRoom.room;
 
 	socket.on('chatMessageServer', function(data) {
+		if (!data || !io.sockets.adapter.rooms[data.room]) {
+			console.log('Invalid data')
+			return;
+		}
+		chatHistory[data.room].push(new ChatData(data.room, data.id, data.msg)); // save message
 		io.to(data.room).emit('chatMessageClient', data);
 	});
 
+	// After user registration
 	socket.on('user reg', function(data, callback) {
-		user.name = data.name;
+		user.name = data.name; // user who registered
+		for (var id in userNames) {
+			var buddy = userNames[id];
+			let room = createRoom([user, buddy]);
+			chatHistory[room] = [];
+			user.buddies[buddy.id] = room;
+			buddy.buddies[user.id] = room;
+			buddy.socket.emit('addUserOnline', {
+				id: user.id,
+				name: user.name,
+				room: room
+			});
+		}
 		userNames[user.id] = user;
-		socket.join('default');
-		callback(user.id);
-		io.emit('users online', onlineUsers());
-	});
 
-	socket.on('userToUserServer', function(userID, callback) { // id of other user
-		var room = createRoom([
-			userNames[user.id],
-			userNames[userID]
-		]);
-		socket.broadcast.to(room).emit('userToUserClient', {
-			userID: user.id,
-			room: room
+		callback({
+			id: user.id,
+			buddies: user.buddies,
+			name: user.name,
+			onlineUsers: onlineUsers()
 		});
-		callback(room);
 	});
 
-	socket.on('createRoom', function(buddy, callback) {
+	socket.on('createGroup', function(buddies, callback) {
 		var users = [];
-		buddy.forEach(function(id) {
+		buddies.forEach(function(id) {
 			users.push(userNames[id]);
 		});
 		var room = createRoom(users);
-		callback(room);
+		for (var user of users) {
+			user.buddies[room] = room;
+		}
+
+		var names = '';
+		for (var user of users) {
+			names += user.name + ', ';
+		}
+		names = names.substring(0, names.length - 2); // removes last comma
+
+		userNames[room] = new User(room, socket, names); // socket irrelevent
+		userNames[room].buddies[room] = room;
+		chatHistory[room] = [];
+		io.to(room).emit('addUserOnline', {
+			id: room,
+			room: room,
+			name: names
+		});
+		callback();
 	});
 
 	socket.on('disconnect', function() {
-		user.rooms.forEach(function(room) {
-			socket.leave(room);
-		});
+		// user.rooms.forEach(function(room) {
+		// 	socket.leave(room);
+		// });
 		delete userNames[user.id];
 		io.emit('users online', onlineUsers());
 	});
@@ -78,9 +131,17 @@ io.on('connection', function(socket) {
 			let user = userNames[id];
 			users.push({
 				id: user.id,
-				name: user.name
+				name: user.name,
+				buddies: user.buddies
 			});
 		}
+
+		users.push({
+			id: publicRoom.id,
+			name: publicRoom.name,
+			buddies: {[publicRoom.id]: publicRoom.room}
+		});
+
 		return users;
 	}
 
@@ -89,13 +150,13 @@ io.on('connection', function(socket) {
 		for (var user of users) {
 			names += user.name;
 		}
-		const hash = require('crypto')
+		const room = require('crypto')
 			.createHash('md5').update(names).digest('hex');
 
 		for (var user of users) {
-			user.socket.join(hash);
-			user.rooms.push(hash);
+			user.socket.join(room);
+			user.rooms.push(room);
 		}
-		return hash;		
+		return room;
 	}
 });
--- public/script.js
+++ public/script.js
@@ -1,90 +1,53 @@
 'use strict';
 
-function User(id, name) {
+function User(id, name, socket, buddies) {
 	this.id = id;
 	this.name = name;
-	this.buddy = {};
+	this.socket = socket;
+	this.buddies = buddies; // room nos
 }
 User.prototype.sendChatMsg = function (msg, room) {
-	socket.emit('chatMessageServer', {
+	this.socket.emit('chatMessageServer', {
 		id  : this.id,
 		name: this.name,
 		msg : msg,
 		room: room
 	});
 };
-User.prototype.userToUser = function(userID) { // create channel for user to user chat
-	var user = this;
-	if (!user.buddy[userID]) {
-		socket.emit('userToUserServer', userID, function(room) {
-			rooms[room] = [];
-			user.buddy[userID] = room;
-			user.openChatWindow(room);
-		});
-	} else {
-		user.openChatWindow(user.buddy[userID]);
-	}
-};
-
-var socket = io();
-var lastID = null;
-var rooms = {default: []}; // stores chat msg for all rooms
-var user = null;
-var publicRoom = {
-	id: 'public-room',
-	name: 'Public Room',
-	room: 'default',
-};
-var curRoom = publicRoom.room;
 
 $(function() {
+	var socket = io();
+	var lastID = null;
+	var rooms = {}; // stores chat msg for all rooms
+	var user = null; // this user
+	var curRoom = null;
+	var publicRoom = null;
+
+	socket.on('init', function(data) {
+		publicRoom = data.publicRoom;
+		curRoom = publicRoom.room;
+		rooms[publicRoom.room] = [];
+	});
+
 	socket.on('chatMessageClient', function(data) {
 		var room = data.room;
 		rooms[room].push(data);
-		if ($('#'+data.room).length) { // if msg is sent from curRoom
-		renderSingleMsg(data);
+		if ($('#'+data.room+'-chat-form').length > 0) { // if msg is sent from curRoom
+			renderSingleMsg(data);
 		} else {
-			if (data.room == publicRoom.room) data.id = publicRoom.id;
-			$('#'+data.id + ' .fa-envelope').removeClass('invisible');
+			$('#'+data.room + ' .fa-envelope').removeClass('invisible');
 		}
 	});
 
 	socket.on('users online', function(users) {
-		users.push({
-			id: publicRoom.id,
-			name: publicRoom.name
-		});
-		$('#users').empty();
-		$(users).each(function() {
-			var userID = this.id;
-			if (user && user.id == userID) return true;
-			$('#users').append(
-				$('<li id="'+userID+'" class="list-group-item">\
-					<label class="form-check-inline invisible">\
-						<input class="form-check-input" type="checkbox" value="'+userID+'">\
-					</label>'
-				 	+ this.name +
-					'<i class="fa fa-envelope pull-right invisible"></i>\
-				 </li>')
-				.click(function() {
-					if (user) {
-						$(this).find('.fa-envelope').addClass('invisible');
-						user.userToUser(userID);
-					}
-				})
-			);
-		});
+		if (!user) return; // will be removed later
+
+		showOnlineList(users);
 	});
 
-	// event after createRoom emit
-	// socket.on('newUserJoined', function(data) {
-	// 	rooms[data.room] = [];
-	// 	onlineUsers[data.id].room = data.room;
-	// });
-	socket.on('userToUserClient', function(data) {
-		rooms[data.room] = [];
-		user.buddy[data.userID] = data.room;
-		$('#'+data.userID + ' .fa-envelope').removeClass('invisible');
+	socket.on('addUserOnline', function(newUser) {
+		user.buddies[newUser.id] = newUser.room;
+		addUserToOnlineList(newUser.id, newUser.room, newUser.name);
 	});
 
 	$('#name').submit(function(event) { // submits user name
@@ -98,12 +61,13 @@ $(function() {
 			});
 		});
 		var name = $('#username').val();
+
 		socket.emit('user reg', {
 			name: name,
-		}, function(id) {
-			user = new User(id, name);
-			user.buddy[publicRoom.id] = publicRoom.room
-			console.log('personID=' + id);
+		}, function(data) {
+			user = new User(data.id, name, socket, data.buddies);
+			showOnlineList(data.onlineUsers);
+			console.log('personID='+data.id);
 		});
 		return false;
 	});
@@ -112,7 +76,7 @@ $(function() {
 		curRoom = room;
 		$('.messages')
 		.empty()
-		.attr('id', room);
+		.attr('id', room+'-chat-form');
 
 		//loads chat messages
 		lastID = null;
@@ -122,7 +86,7 @@ $(function() {
 	}
 
 	function renderSingleMsg(data) {
-		var $id = $('#' + data.room);
+		var $id = $('#'+data.room+'-chat-form');
 		var outline = 'primary';
 		var tag = 'primary';
 		if (data.id == user.id) {
@@ -140,5 +104,68 @@ $(function() {
 		lastID = data.id;
 	}
 
-	User.prototype.openChatWindow = openChatWindow;
+	function showOnlineList(users) {
+		$('#users').empty();
+		$(users).each(function() {
+			var userID = this.id;
+			if (user.id == userID) {
+				user.buddies = this.buddies;
+				return true; // continue statement
+			}
+
+			addUserToOnlineList(userID, user.buddies[userID], this.name);
+		});
+	}
+
+	function addUserToOnlineList(userID, room, name) {
+		if (!rooms[room]) {
+			rooms[room] = [];
+		}
+		$('#users').append(
+			$('<li id="'+room+'" class="list-group-item">\
+				<label class="form-check-inline invisible">\
+					<input class="form-check-input" type="checkbox" value="'+userID+'">\
+				</label>'
+				+ name +
+				'<i class="fa fa-envelope pull-right invisible"></i>\
+			 </li>')
+			.click(function() {
+				$(this).find('.fa-envelope').addClass('invisible'); // hides fa-envelope
+				openChatWindow(room);
+			})
+		);
+	}
+
+	$("#add-users").click(function() { // .fa-user-plus
+		$('#users .form-check-inline').removeClass('invisible');
+		$(this).prop('hidden', true);
+		$('#create-room').prop('hidden', false);
+	});
+
+	$('#create-room').click(function() {
+		var buddies = [user.id];
+		$("#users :checkbox:checked").each(function() {
+			buddies.push($(this).val());
+		});
+		if (buddies.length == 1) {
+			showAlert('Select atleast 1 person');
+			return;
+		}
+		socket.emit('createGroup', buddies, function() {
+			$("#add-users").prop('hidden', false);
+			$('#create-room').prop('hidden', true);
+			$('#users .form-check-inline').addClass('invisible');
+		});
+	});
+
+	function showAlert(msg) {
+		$('#chat-box').prepend(
+			'<div class="alert alert-danger alert-dismissible show" role="alert">\
+  				<button type="button" class="close" data-dismiss="alert" aria-label="Close">\
+    				<span aria-hidden="true">&times;</span>\
+  				</button>'
+  				+ msg +
+			'</div>'
+		);
+	}
 });
